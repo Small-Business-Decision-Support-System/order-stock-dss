1b63792619aad67fb66f39407e083ebb3f809aa4
Abdourahman Youssouf Ibrahim <bouleau457@gmail.com>
2026-04-22 15:10:56 +0300

    Update database.py

--- database.py
+++ database.py
@@ -6,10 +6,11 @@ import psycopg2
 def get_connection():
     # this function creates a new connection to the database every time it's called
     conn = psycopg2.connect(
-        host="localhost",        # the server where our database lives
-        database="inventory_db", # the name of our database
-        user="postgres",         # the username to access the database
-        password="your_password" # the password — Sedra will give you this
+        host="localhost",        
+        database="inventory_db",
+        user="postgres",         
+        password="your_password" 
+        #Sedra will give me this
     )
     return conn  
-    # we return the connection so other files can use it
+    
